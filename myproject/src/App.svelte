271f93d2ac72d9dd50d9f1f55a10e1aaf80e3d6a
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-18 21:26:38 +0530

    Inline filters

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -4,14 +4,25 @@
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
     { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
   	]
+
+	const handleClick = (e,id) => {
+		// have to reassign or else it won't update the UI and filter the people array 
+		people = people.filter((person) => person.id != id)
+		console.log(e)
+	}
 </script>
 
 <main>
-	{#each people as person}
+	{#each people as person (person.id)}
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColour} belt.</p>
+			
+			<!-- on click if we directly call the fuction till will excute in runtime so making () => works  -->
+			<button on:click={(e) => handleClick(e, person.id)}> Delete </button>
 		</div>
+		{:else}
+		<p>There are no people to show</p>
 	{/each}
 </main>
 
