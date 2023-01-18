9f1c94c210601848ce289d9d06ab2deafcd2b5ed
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-18 21:50:42 +0530

    Svelete Components

---
+++ loginPage/src/Modal.svelte
@@ -0,0 +1,14 @@
+<script>
+    let showModal = true
+</script>
+
+{#if showModal}
+<div>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</div>
+{/if}
+
+
+<style>
+</style>
