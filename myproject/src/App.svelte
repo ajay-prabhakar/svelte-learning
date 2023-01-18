d2d324c85423cea53182ba4d26f89e0ae0d74a5a
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-18 21:13:49 +0530

    Components loops

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,31 +1,18 @@
 <script>
-	let  firstName = 'Yoshi'
-	let  lastName = 'Yoshi'
-
-	let beltcolor = "black"
-	$: fullName = `${firstName} ${lastName}`
-
-	var name = firstName + lastName
-
-	const handleClick = () => {
-		beltcolor = 'orange'
-	}
-
-	const handleInput = (e) => {
-		beltcolor = e.target.value
-	}
+	let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  	]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit this fuck {beltcolor}.</p>
-	<button on:click={handleClick}>This is belt color</button>
-	<!-- <input type="text" on:input={handleInput} value={beltcolor}> -->
-
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-
-	<input type="text" bind:value={beltcolor}>
+	{#each people as person}
+		<div>
+			<h4>{person.name}</h4>
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+		</div>
+	{/each}
 </main>
 
 <style>
