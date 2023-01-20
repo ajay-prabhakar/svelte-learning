636daab7409fb53d3d734ed0040eacae40fe4e95
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-20 13:13:39 +0530

    Add Current polls component

--- sveltePoll/src/App.svelte
+++ sveltePoll/src/App.svelte
@@ -3,12 +3,30 @@
 	import Footer from "./Components/Footer.svelte";
 	import Tabs from "./Shared/Tabs.svelte";
 	import CreatePollForm from "./Components/CreatePollForm.svelte";
+	import PollList from "./Components/PollList.svelte";
 
 	let items = ["Current Polls", "Add a new Poll"]
 	let activeItem = "Current Polls"
 
+	let polls = [
+		{
+			id: 1, 
+			question: 'Phython or javaScript',
+			answerA: 'Phython',
+			answerb: 'JavaScript',
+			votesA: 9, 
+			votesB: 15,
+		}
+	]
+
 	const tabChange = (e) =>{
 		activeItem = e.detail
+	}
+
+	const addPoll = (e) => {
+		let poll = e.detail
+		polls = [poll,...polls]
+		activeItem = items[0]
 	}
 </script>
 
@@ -16,9 +34,9 @@
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange}></Tabs>
 	{#if activeItem=== items[0]}
-	<p>Poll component goes here</p>
+	<PollList {polls}></PollList>
 	{:else if activeItem === items[1]}
-	<CreatePollForm></CreatePollForm>
+	<CreatePollForm on:add = {addPoll}></CreatePollForm>
 	{/if}
 </main> 
 <Footer></Footer>
--- sveltePoll/src/Components/CreatePollForm.svelte
+++ sveltePoll/src/Components/CreatePollForm.svelte
@@ -1,5 +1,7 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
     import Button from '../shared/Button.svelte';
+    let dispatch = createEventDispatcher()
   
     let fields = { question: '', answerA: '', answerB: '' };
     let errors = { question: '', answerA: '', answerB: '' };
@@ -30,7 +32,8 @@
       }
       // add new poll
       if (valid) {
-        console.log('valid', fields);
+        let poll = {...fields, votesA: 0, votesB: 0, id: Math.random}
+        dispatch('add', poll)
       }
     }
   </script>
