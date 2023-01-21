d0b63c1c548296c89c6c6654a515bcb977efb5a7
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-21 10:26:53 +0530

    dynamic color change of poll

--- sveltePoll/src/Components/PollDetails.svelte
+++ sveltePoll/src/Components/PollDetails.svelte
@@ -1,63 +1,78 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from 'svelte';
 
+  export let poll;
+  const dispatch = createEventDispatcher();
 
-    let dispatch = createEventDispatcher()
-    export let poll;
-  
-    // reactive values
-    $: totalVotes = poll.votesA + poll.votesB;
+  // reactive values
+  $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor(100 / totalVotes * poll.votesA) || 0;
+  $: percentB = Math.floor(100 / totalVotes * poll.votesB) || 0;
 
-    const handleVote = (option, id) => {
-        dispatch("vote", {option: option, id: id})
-    }
-  </script>
-  
-  <div class="card">
+  // handling votes
+  const handleVote = (option, id) => {
+    dispatch('vote', {option, id});
+  };
+</script>
+
+<div class="card">
   <div class="poll">
     <h3>{ poll.question }</h3>
     <p>Total votes: { totalVotes }</p>
-    <!-- svelte-ignore missing-declaration -->
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('a', poll.id)}>
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%"></div>
       <span>{ poll.answerA } ({ poll.votesA } votes)</span>
     </div>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote('b', poll.id)}>
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentB}%"></div>
       <span>{ poll.answerB } ({ poll.votesB } votes)</span>
     </div>
   </div>
 </div>
-  
+
 <style>
-    h3{
-      margin: 0 auto;
-      color: #555;
-    }
-    p{
-      margin-top: 6px;
-      font-size: 14px;
-      color: #aaa;
-      margin-bottom: 30px;
-    }
-    .answer{
-      background: #fafafa;
-      cursor: pointer;
-      margin: 10px auto;
-      position: relative;
-    }
-    .answer:hover{
-      opacity: 0.6;
-    }
-    span{
-      display: inline-block;
-      padding: 10px 20px;
-    }
-    .card{
+  h3{
+    margin: 0 auto;
+    color: #555;
+  }
+  p{
+    margin-top: 6px;
+    font-size: 14px;
+    color: #aaa;
+    margin-bottom: 30px;
+  }
+  .answer{
+    background: #fafafa;
+    cursor: pointer;
+    margin: 10px auto;
+    position: relative;
+  }
+  .answer:hover{
+    opacity: 0.6;
+  }
+  span{
+    display: inline-block;
+    padding: 10px 20px;
+  }
+  .percent{
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a{
+    background: rgba(217,27,66,0.2);
+    border-left: 4px solid #d91b42;
+  }
+  .percent-b{
+    background: rgba(69,196,150,0.2);
+    border-left: 4px solid #45c496;
+  }
+  .card{
     background: #fff;
     padding: 20px;
     border-radius: 6px;
     box-shadow: 0px 2px 4px rgba(0,0,0,0.1);
-    }
+  }
 </style>
