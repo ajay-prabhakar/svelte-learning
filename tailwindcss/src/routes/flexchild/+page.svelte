f083f78a4c31cf6f71884311a895f294cb0c4d15
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-01-24 14:50:51 +0530

    flexbox of child

---
+++ tailwindcss/src/routes/flexchild/+page.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col justify-between items-strech h-80 bg-slate-400 mb-4">
+	<div class="p-2 bg-red-600">This is block 1 okay below the text</div>
+	<div class="p-2 bg-yellow-500 text-5xl">This is block 2</div>
+	<div class="p-2 bg-blue-500">This is block 1</div>
+	<div class="p-2 bg-violet-600 self-center">This is block 1</div>
+</div>
+
+<div class="flex justify-between items-strech bg-slate-400 mb-4">
+	<div class="p-2 bg-red-600 flex-shrink-0">
+		This is block 1 this is the best one have to disasasdasd
+	</div>
+	<div class="p-2 bg-yellow-500 text-5xl flex-grow">This is block 2</div>
+	<div class="p-2 bg-blue-500">This is block 1</div>
+	<div class="p-2 bg-violet-600">This is block 1</div>
+</div>
+
+<div class="flex justify-between items-strech bg-slate-400 mb-4 space-x-2">
+	<div class="p-2 bg-red-600">This is block 1 this is the best one have to disasasdasd</div>
+	<div class="p-2 bg-yellow-500 text-5xl">This is block 2</div>
+	<div class="p-2 bg-blue-500">This is block 1</div>
+	<div class="p-2 bg-violet-600">This is block 1</div>
+</div>
